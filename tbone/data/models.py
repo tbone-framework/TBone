78a9ee2e4cdee26accc119bf08e4f49d75c4b386
Amit Nabarro <amit.nabarro@gmail.com>
2017-08-06 12:29:51 +0300

    Export decorator for model export methods

--- tbone/data/models.py
+++ tbone/data/models.py
@@ -3,19 +3,40 @@
 
 from collections import OrderedDict
 from .fields import BaseField
+from functools import wraps
 
 
 class ModelMeta(type):
     '''Metaclass for Model'''
-    def __new__(mcs, name, bases, attrs):
-        fields = OrderedDict()
+    @classmethod
+    def __prepare__(mcl, name, bases):
+        def export(func):
+            func._export_method_ = True
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                return func(*args, **kwargs)
 
+            return wrapper
+        d = dict()
+        d['export'] = export
+        return d
+
+    def __new__(mcl, name, bases, attrs):
+        del attrs['export']
+        fields = OrderedDict()
+        exports = OrderedDict()
+
+        # collect all defined fields and export methods
         for key, value in attrs.items():
+            if getattr(value, '_export_method_', None):
+                exports[key] = value
+                
             if isinstance(value, BaseField):
                 fields[key] = value
 
         attrs['_fields'] = fields
-        cls = super(ModelMeta, mcs).__new__(mcs, name, bases, attrs)
+        attrs['_exports'] = exports
+        cls = super(ModelMeta, mcl).__new__(mcl, name, bases, attrs)
         for name, field in fields.items():
             field.add_to_class(cls, name)
         return cls
@@ -24,18 +45,33 @@ class ModelMeta(type):
 class Model(object, metaclass=ModelMeta):
 
     def __init__(self, data={}, **kwargs):
-        self._data = self._convert(data)
+        self._data = {}
+        if bool(data):
+            self.import_data(data)
+            self.validate()
+
 
     def __repr__(self):
         return '<%s instance>' % self.__class__.__name__
 
     def to_data(self):
+        '''
+        Convert all data in model from python data types to simple form for serialization.
+
+        '''
         data = {}
+        # iterate through all fields 
         for field_name, field in self._fields.items():
-            if field_name in self._data:
-                data[field_name] = field.to_data(self._data[field_name])
+            field_data = field.to_data(self._data.get(field_name))
+            if field_data:
+                data[field_name] = field_data
             elif field._export_if_none is True:
                 data[field_name] = None
+
+        # iterate through all exports
+        for name, func in self._exports.items():
+            data[name] = func(self)
+
         return data
 
     def to_python(self):
@@ -51,17 +87,14 @@ class Model(object, metaclass=ModelMeta):
         self._validate(self._data)
 
     def _convert(self, data):
+        ''' Converts given data from primitive form to Python variables and objects '''
         converted_data = {}
         for name, field in self._fields.items():
-            if name in data:  # data contains the field's name as key
-                converted_data[name] = field.to_python(data[name])
-            else:  # data does not contain the field's name
-                d = field.default
-                if d is not None:
-                    converted_data[name] = field.to_python(d)
-                else:
-                    pass  # TODO: add serialized when None
+            converted_data[name] = field.to_python(data.get(name))
         return converted_data
+
+    def import_data(self, data):
+        self._data = self._convert(data)
 
     @classmethod
     def _export(self, data):
--- tests/test_models.py
+++ tests/test_models.py
@@ -29,9 +29,6 @@ def test_model_creation_and_export():
     # m = M({'name': 'Ron Burgundy', 'age': 45, 'decimal': 34.77, 'dt': datetime.datetime.utcnow()})
     m = M({'name': 'Ron Burgundy', 'age': 45, 'decimal': '34.77', 'dt': '2017-07-25T12:34:14.414471'})
 
-
-    import pdb; pdb.set_trace()
-
     # convert model to primitive form
     prim = m.to_data()
     # check result is dict
@@ -39,4 +36,19 @@ def test_model_creation_and_export():
     # check keys match
     assert all(a == b for a, b in zip_longest(m._fields.keys(), prim.keys(), fillvalue=None))
 
+
+def test_model_export_decorator():
+    class M(Model):
+        first_name = StringField()
+        last_name = StringField()
+
+        @export
+        def full_name(self):
+            return '{} {}'.format(self.first_name, self.last_name)
+
+    m = M({'first_name': 'Ron', 'last_name': 'Burgundy'})
+    data = m.to_data()
+
+    assert 'full_name' in data
+    assert data['full_name'] == 'Ron Burgundy'
 
