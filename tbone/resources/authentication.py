a2f58aa5cad277e8c2f52895e6bb1f6cba77f314
Amit Nabarro <amit.nabarro@gmail.com>
2017-09-18 09:36:34 +0300

    async authentication methods

--- tbone/resources/authentication.py
+++ tbone/resources/authentication.py
@@ -3,12 +3,12 @@
 
 
 class NoAuthentication(object):
-    def is_authenticated(self, request):
+    async def is_authenticated(self, request):
         return True
 
 
 class ReadOnlyAuthentication(NoAuthentication):
-    def is_authenticated(self, request):
+    async def is_authenticated(self, request):
         if request.method.upper() == 'GET':
             return True
         return False
